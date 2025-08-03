3f1e9c3b0d9db8f4bb7efd14bb2efa40af232ecf
Nitesh Warik <niteshwarik23@gmail.com>
2025-08-04 00:26:17 +0530

    "Nitesh - Download resume asset path changes as per build web folder"

--- lib/widgets/download_resume_button.dart
+++ lib/widgets/download_resume_button.dart
@@ -7,7 +7,9 @@ class ResumeButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
       onPressed: () {
-        final url = 'assets/Nitesh_Warik_Flutter.pdf'; // Relative URL in build/web
+        final url =
+            //'https://drive.google.com/uc?export=download&id=1JK7-0N8hh4r6KWLQ3qtGTJgWixpCaWQ_';
+        'assets/assets/Nitesh_Warik_Flutter.pdf'; // Relative URL in build/web
         html.AnchorElement anchorElement = html.AnchorElement(href: url)
           ..setAttribute("download", "Nitesh_Warik_Flutter.pdf")
           ..click();
@@ -22,11 +24,12 @@ class ResumeButton extends StatelessWidget {
           ),
         ),
       ),
-       icon: Icon(
+      icon: Icon(
         Icons.file_download_rounded,
         color: Color(0xffE99287),
       ),
-      label: Text("Download Resume",
+      label: Text(
+        "Download Resume",
         style: GoogleFonts.firaCode(
           color: Color(0xffE99287),
           fontSize: 16,
@@ -35,4 +38,5 @@ class ResumeButton extends StatelessWidget {
     );
   }
 }
-// download_resume_button.dart resume was not available on site error is shown when trying to download the resume
+// i used the assets path but when deployed to github pages it does not work
+// because the pdf is in build web in assets/assets/Nitesh_Warik_Flutter.pdf
